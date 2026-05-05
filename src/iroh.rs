ba46f974cd32fa1636f316b61189aefda2e67506
Denis S <deni087607@gmail.com>
2026-05-05 06:21:00 +0300

    refactor(iroh): rewrite iroh module to state machine for better testability and avoiding data races

--- src/iroh.rs
+++ src/iroh.rs
@@ -18,13 +18,19 @@ pub struct Iroh {
 	rx_from_coord: mpsc::Receiver<CoreAction>,
 	tx_to_manager: mpsc::Sender<ConnectionEvent>,
 	rx_from_tasks: mpsc::Receiver<ConnectionEvent>,
-	state_table: HashMap<PublicKey, Option<ConnectionState>>,
+	state_table: HashMap<PublicKey, ConnectionState>,
 	peers: HashSet<PublicKey>,
 }
 
-struct ConnectionState {
-	task: JoinHandle<()>,
-	tx_packet: mpsc::Sender<Bytes>,
+enum ConnectionState {
+	Connected {
+		task: JoinHandle<()>,
+		connection: Connection,
+		tx_packet_channel: mpsc::Sender<Bytes>,
+	},
+	Connecting(JoinHandle<()>),
+	WaitingIncomingConnection,
+	Disconnected,
 }
 
 #[derive(Debug, PartialEq)]
@@ -37,6 +43,7 @@ pub enum IrohEvent {
 #[derive(Debug, PartialEq)]
 pub enum CoreAction {
 	SendPacketTo(Bytes, PublicKey),
+	ConnectToPeer(PublicKey),
 	DisconnectFromPeer(PublicKey),
 }
 
@@ -73,9 +80,9 @@ impl Iroh {
 			   		 listen_port: u16) 
 		-> Result<Self, InitError> 
 	{
-		let mut state_table: HashMap<PublicKey, Option<ConnectionState>> = HashMap::with_capacity(peers.len());
+		let mut state_table: HashMap<PublicKey, ConnectionState> = HashMap::with_capacity(peers.len());
 		for pub_key in &peers {
-			state_table.insert(pub_key.clone(), None);
+			state_table.insert(pub_key.clone(), ConnectionState::Disconnected);
 		}
 
 		let builder = Endpoint::builder(presets::N0)
@@ -138,33 +145,128 @@ impl Iroh {
 		}
 	}
 
+	#[instrument(skip_all)]
 	async fn handle_conn_event(&mut self, event: ConnectionEvent) -> Result<(), ()> {
 		match event {
 			ConnectionEvent::OutgoingConnection(conn) => {
-				self.init_connection(conn).await;
+				match self.state_table.get(&conn.remote_id()) {
+					Some(state) => match state {
+						// in queue possible 2 outgoing
+						// for example {DisconnectCore, ConnectCore,
+						// ... here already produced another outgoing connection from another task ..., 
+						// OutgoingConnection}
+						
+						// so it is comes down to 
+						// {OutgoingConn(pub_key_x), OutgoingConn(pub_key_x)}
+						// so other peer accept the second one probably
+						
+						// can he accept the first one??? - no, because the first one change dist peer state to connected,
+						// after that he receive the another conn, accept it
+
+						// he definitely accept the second one
+						// so here we can do the same - accept the second if we already connected  
+						ConnectionState::Connecting(_) => {
+							self.init_ready_connection(conn).await;
+						}
+
+						ConnectionState::Connected{task: expired_task, connection: expired_conn, ..} => {
+							info!("data race: receive outgoing connecting while current state is Connected");
+							expired_task.abort();
+							expired_conn.close(1u32.into(), b"connection is expired: data race resolver");
+
+							self.init_ready_connection(conn).await;	
+						}
+
+						ConnectionState::WaitingIncomingConnection => {
+							// base invariant of order of public keys
+							conn.close(1u32.into(), b"peer violated public keys invariant");
+							warn!("weird connection event: 
+									receive outgoing connecting while current state is WaitingIncomingConnection");
+						}
+
+						// possible when this event come early then Disconnect from core was handled 
+						ConnectionState::Disconnected => {
+							conn.close(1u32.into(), b"can't connect right now");
+							debug!("ignored outgoing connection: current state is Disconnected");
+						}	
+					}
+					
+					None => {} // HANDLE THIS
+				}
 			}
+			
 			ConnectionEvent::IncomingConnection(conn) => {
-				if let Some(Some(ConnectionState {task: old_task, ..})) = self.state_table.get(&conn.remote_id()) {
-					old_task.abort();
-					info!("New incoming connection replace previous task");
-				}
+				match self.state_table.get(&conn.remote_id()) {
+					Some(state) => match state {
+						ConnectionState::WaitingIncomingConnection => {
+							// {DisconnectCore, ConnectCore - state = Waiting, IncomingConnection} - it's ok
+							// in that queue can be multiple Incoming, so just accept the latest one
+							self.init_ready_connection(conn).await;		
+						}
 
-				self.init_connection(conn).await;
+						ConnectionState::Connected{task: expired_task, connection: expired_conn, ..} => {
+							// already handle Incoming, so in queue 2 incoming, 
+							// this should be impossible, without expired connection or data race on another peer
+
+							// also possible ddos
+							info!("New incoming connection replace previous task");
+							expired_task.abort();
+							expired_conn.close(1u32.into(), b"connection is expired: peer open another connection");
+
+							self.init_ready_connection(conn).await;		
+						}
+
+						ConnectionState::Connecting(_) => {
+							// base invariant of order of public keys
+							conn.close(1u32.into(), b"peer violated public keys invariant");
+							warn!("weird connection event: receive incoming connection while current state is Connecting");
+						}
+
+						// possible when this event come early then Disconnect from core was handled
+						ConnectionState::Disconnected => {
+							conn.close(1u32.into(), b"can't connect right now");
+							debug!("ignored incoming connection: current state is Disconnected");
+						}	
+					}
+					
+					None => {} // HANDLE THIS
+				}
 			}
+			
+			// invariants done
 			ConnectionEvent::Disconnected(pub_key) => {
-				let _ = self.tx_to_coord.send(
-					IrohEvent::DisconnectedFromPeer(pub_key.clone())
-				).await;
-				self.state_table.insert(pub_key.clone(), None);
+				match self.state_table.get(&pub_key) {
+					Some (state) => match state {
+						ConnectionState::Connected{..} => {
+							// {DisconnectCore, ConnectCore, already connected (it's not possible,
+							// because we handle the ConnctedToPeer after Disconnect from task), Disconnect from task}
+							// everything ok, it's correct disconnect for CURRENT connection
+							let _ = self.tx_to_coord.send(
+								IrohEvent::DisconnectedFromPeer(pub_key.clone())
+							).await;
+							self.init_new_connection(pub_key);
+						}
 
-				if self.endpoint.id() < pub_key || !self.state_table.contains_key(&pub_key) {
-					return Ok(());
+						ConnectionState::Connecting(_) => {
+							// possible queue {DisconnectCore, ConnectCore, Disconnected from task}
+							// so nothing to do
+							debug!("data race: receive ConnectionEvent::Disconnected while current state is Connecting");
+						}
+
+						ConnectionState::WaitingIncomingConnection => {
+							// same as Connecting
+							debug!("data race: receive ConnectionEvent::Disconnected 
+								    while current state is WaitingIncomingConnection");
+						}
+
+						ConnectionState::Disconnected => {
+							// also possible {DisconnectCore, Disconnect from task}
+							debug!("data race: receive ConnectionEvent::Disconnected while current state is Disconnected")
+						}
+					}
+					
+					None => {} // HANDLE THIS
 				}
-				tokio::spawn(
-					Self::connect_to_peer(self.endpoint.clone(),
-										  pub_key,
-										  self.tx_to_manager.clone())
-				);
 			}
 			ConnectionEvent::InternalError => {
 				return Err(());
@@ -174,11 +276,12 @@ impl Iroh {
 		Ok(())
 	}
 
+	#[instrument(skip_all)]
 	async fn handle_core_event(&mut self, event: CoreAction) {
 		match event {
 			CoreAction::SendPacketTo(packet, to) => {
-				if let Some(Some(ConnectionState {tx_packet, ..})) = self.state_table.get(&to) {
-					if let Err(e) = tx_packet.try_send(packet) {
+				if let Some(ConnectionState::Connected{tx_packet_channel, ..}) = self.state_table.get(&to) {
+					if let Err(e) = tx_packet_channel.try_send(packet) {
 					    debug!(to = %to, "Peer queue is full, dropping packet");
 					}
 					return;
@@ -186,14 +289,50 @@ impl Iroh {
 				// it is possible when the core didn't get the message about disconnect in time
 				debug!(size = packet.len(), "Drop packet from tun");
 			}
-			
-			// impossible in current implementation of iroh + core connect managment 
-			CoreAction::DisconnectFromPeer(pub_key) => {
-				if let Some(Some(ConnectionState {task: old_task, ..})) = self.state_table.get(&pub_key) {
-					old_task.abort();
-				}
+		
+			CoreAction::ConnectToPeer(pub_key) => {
+				match self.state_table.get(&pub_key) {
+					Some(state) => match state {
+						ConnectionState::Disconnected => {
+							self.init_new_connection(pub_key);
+						}
 
-				let _ = self.state_table.remove(&pub_key); // incorrect state from core
+						ConnectionState::Connected{..} => {
+							warn!("weird action from core: ConnectToPeer while current state is Connected");
+						}
+
+						ConnectionState::Connecting(_) => {
+							warn!("weird action from core: ConnectToPeer while current state is Connecting");
+						}
+
+						ConnectionState::WaitingIncomingConnection => {
+							warn!("weird action from core: ConnectToPeer while current state is WaitingIncomingConnection");
+						}
+					}
+					
+					None => {} // HANDLE THIS
+				}
+			}
+			 
+			CoreAction::DisconnectFromPeer(pub_key) => {
+				match self.state_table.get(&pub_key) {
+					Some(state) => match state {
+						ConnectionState::Connected{task: old_task, connection: old_conn, ..} => {
+							old_task.abort();
+							old_conn.close(1u32.into(), b"disconnect from peer core action");
+						}
+
+						ConnectionState::Connecting(conn_task) => {
+							conn_task.abort();
+						}
+
+						_ => {}						
+					}
+					
+					None => {}
+				}	
+
+				self.state_table.insert(pub_key.clone(), ConnectionState::Disconnected);
 
 				let _ = self.tx_to_coord.send(
 					IrohEvent::DisconnectedFromPeer(pub_key)
@@ -202,36 +341,57 @@ impl Iroh {
 		}
 	}
 
-	async fn init_connection(&mut self, conn: Connection) {
+	// new state: Connecting or WaitingIncomingConnection
+	fn init_new_connection(&mut self, pub_key: PublicKey) {
+		if self.endpoint.id() < pub_key {
+			self.state_table.insert(pub_key, ConnectionState::WaitingIncomingConnection);
+			return;
+		}
+
+		let task = tokio::spawn(
+			Self::connect_to_peer(self.endpoint.clone(),
+								  pub_key.clone(),
+								  self.tx_to_manager.clone())
+		);
+
+		self.state_table.insert(pub_key, ConnectionState::Connecting(task));
+	}
+
+	// new state: Connected
+	async fn init_ready_connection(&mut self, conn: Connection) {
 		let _ = self.tx_to_coord.send(IrohEvent::ConnectedToPeer(conn.remote_id())).await;
-		let (tx_packet, rx_packet_channel) = mpsc::channel(CHANNEL_CAPACITY);
+		let (tx_packet_channel, rx_packet_channel) = mpsc::channel(CHANNEL_CAPACITY);
 		let task = tokio::spawn(
 			Self::on_peer_connected(self.tx_to_coord.clone(),
 									rx_packet_channel,
 									conn.clone(),
 									self.tx_to_manager.clone())
 		);
-		self.state_table.insert(conn.remote_id(), Some(ConnectionState {
-			task,
-			tx_packet,
-		}));
+		self.state_table.insert(conn.remote_id(), ConnectionState::Connected{
+			task: task,
+			connection: conn,
+			tx_packet_channel,
+		});
 	}
 	
 	fn init(&mut self) {
+		for pub_key in &self.peers {
+			if *pub_key > self.endpoint.id() {
+				self.state_table.insert(pub_key.clone(), ConnectionState::WaitingIncomingConnection);
+				continue;
+			}
+
+			let task = tokio::spawn(
+				Self::connect_to_peer(self.endpoint.clone(), pub_key.clone(), self.tx_to_manager.clone())	
+			);
+			self.state_table.insert(pub_key.clone(), ConnectionState::Connecting(task));
+		}
+		
 		tokio::spawn(
 			Self::accept_connections(self.endpoint.clone(), 
 									 std::mem::take(&mut self.peers), 
 									 self.tx_to_manager.clone())
 		);
-		for (pub_key, _) in &self.state_table {
-			if *pub_key > self.endpoint.id() {
-				continue;
-			}
-
-			tokio::spawn(
-				Self::connect_to_peer(self.endpoint.clone(), pub_key.clone(), self.tx_to_manager.clone())	
-			);
-		}
 	}
 
 	#[instrument(skip_all, fields(peer = %connection.remote_id()))]
@@ -323,10 +483,15 @@ impl Iroh {
 							// check connection
 							let pub_key = conn.remote_id();
 
-							if !peers.contains(&pub_key) || pub_key < endpoint.id() {
-								debug!(peer = %pub_key, "Rejected connection (not in peers list or order check failed)");
+							if !peers.contains(&pub_key) {
+								debug!(peer = %pub_key, "Rejected connection (not in peers list)");
 								continue;
-							}			
+							}
+
+							if pub_key < endpoint.id() {
+								debug!(peer = %pub_key, "Rejected connection (order invariant of public keys are incorrect)");
+								continue;
+							}
 
 							info!(peer = %pub_key, "Accepted new incoming connection");
 							let _ = tx_to_manager.send(ConnectionEvent::IncomingConnection(conn)).await;
